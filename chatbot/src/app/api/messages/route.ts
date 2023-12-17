b419e425cbe3c5701a1ae313fac39bf15794cadd
Tom Le Coney <tleconey@gmail.com>
2023-12-16 18:18:28 -0600

    adding messages and status routes

---
+++ chatbot/src/app/api/messages/route.ts
@@ -0,0 +1,22 @@
+// src/app/api/messages/route.ts
+import { getMessagesForThread, createThreadAndRun } from "@/lib/openai";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const threadId = searchParams.get("threadId");
+  if (!threadId) {
+    return NextResponse.json({ noMessages: true });
+  }
+  const messages = await getMessagesForThread(threadId);
+  return NextResponse.json(messages);
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const { question, threadId } = data;
+  const { run, thread } = await createThreadAndRun(threadId, question);
+  const runId = run.id;
+  const newThreadId = thread.id;
+  return NextResponse.json({ runId, threadId: newThreadId });
+}
